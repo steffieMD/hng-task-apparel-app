125eac838a0341602585b212591eaab082bb8155
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-11 15:54:29 +0100

    Update ProductPage.jsx

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,12 @@
 import Footer from "../Footer";
 import Cart from "../Cart";
+import Navbar from "../Navbar";
 
 export default function ProductPage() {
   return (
     <>
-      {/* <Navbar /> */}
-      <Cart />
+      <Navbar />
+      <div className="container-fluid border"></div>
       <Footer />
     </>
   );
