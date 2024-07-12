c01a61afded46c5b04162aa89c5bf780030fd8ac
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-12 10:18:32 +0100

    Update Navbar styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,25 +10,26 @@ import { Outlet, Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-        <div className="container-fluid gap-4">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="shop logo" className="homePage" />
-          </Link>
-          <div
-            className="collapse navbar-collapse "
-            id="navbarSupportedContent">
-            <ul className="navbar-nav gap-3 ms-auto me-auto mb-2 mb-lg-0 ">
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+          <div className="toggle-div">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <Link className="navbar-brand" to="/">
+              <img src={logo} alt="shop logo" className="homePage" />
+            </Link>
+          </div>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link ps-2" href="#">
                   New In
@@ -56,46 +57,44 @@ export default function Navbar() {
                 <a className="nav-link sale ps-2">Sale</a>
               </li>
             </ul>
-            <form className="d-flex flex-row " role="search">
+            <form class="d-flex flex-row" role="search">
               <input
-                className="form-control "
+                class="form-control "
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn " type="submit">
-                <img src="src/assets/svg/search-icon.svg" alt="" />
+              <button class="btn btn-outline-success" type="submit">
+                <img src={searchIcon} alt="" />
               </button>
             </form>
           </div>
-        </div>
-        <ul
-          className=" me-auto mb-2 mb-lg-0 nav-icons gap-3
-">
-          <li className="nav-item">
-            <a className="nav-link magIcon" href="#">
-              <img src={magnifyingGlassIcon} alt="like icon" />
-            </a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/cartpage">
-              <span className="cart-icon">
-                <img src={cartIcon} alt="cart icon" />
-              </span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <img src={likeIcon} alt="like icon" />
-            </a>
-          </li>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 nav-icons">
+            <li className="nav-item">
+              <a className="nav-link magIcon" href="#">
+                <img src={magnifyingGlassIcon} alt="like icon" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cartpage">
+                <span className="cart-icon">
+                  <img src={cartIcon} alt="cart icon" />
+                </span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img src={likeIcon} alt="like icon" />
+              </a>
+            </li>
 
-          <li className="nav-item">
-            <a className="nav-link ">
-              <img src={profileIcon} alt="profile icon" />
-            </a>
-          </li>
-        </ul>
+            <li className="nav-item">
+              <a className="nav-link ">
+                <img src={profileIcon} alt="profile icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
